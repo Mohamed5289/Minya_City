bea49157291aac222be424558680b9cb349e9175
Mohamed5289 <ragab.168926@gmail.com>
2025-07-14 11:12:32 +0300

    add changes for server.js

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ db.sequelize
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT || 3000, () => {
+app.listen(PORT, '0.0.0.0', () => {
 	console.log('Server is running on http://localhost:3000');
 });
