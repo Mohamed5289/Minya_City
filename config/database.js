ce0880fe4f3fb3581d0d64bfc2110b60a3e36827
Mohamed5289 <ragab.168926@gmail.com>
2025-07-14 10:40:23 +0300

    change in database config

--- config/database.js
+++ config/database.js
@@ -1,27 +1,22 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_ID,
-	process.env.DB_PASSWORD,
-	{
-		host: process.env.DB_HOST,
-		dialect: 'mssql',
-		pool: {
-			max: 100,
-			min: 10,
-			acquire: 30000,
-			idle: 10000,
-		},
-		dialectOptions: {
-			options: {
-				trustedConnection: true,
-				enableArithAbort: true,
-				trustServerCertificate: true,
-			},
+const sequelize = new Sequelize('db23253', 'db23253', process.env.DB_PASSWORD, {
+	host: 'db23253.public.databaseasp.net',
+	dialect: 'mssql',
+	pool: {
+		max: 100,
+		min: 10,
+		acquire: 30000,
+		idle: 10000,
+	},
+	dialectOptions: {
+		options: {
+			trustedConnection: true,
+			enableArithAbort: true,
+			trustServerCertificate: true,
 		},
 	},
-);
+});
 
 module.exports = sequelize;
